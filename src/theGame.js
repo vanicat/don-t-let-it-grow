8d84e82ede74f252164ed1821804b89a12905c0f
Rémi Vanicat <vanicat@debian.org>
2018-06-23 21:20:09 +0200

    rename range display

--- src/theGame.js
+++ src/theGame.js
@@ -1,4 +1,4 @@
-/* global Phaser Range ToolBox */
+/* global Phaser RangeDisplay ToolBox */
 const CAMERA_MOVE = 40
 
 var theGame = function (game) {
@@ -73,17 +73,17 @@ theGame.prototype = {
   createCounter: function () {
     // gold should be numeric, not range...
 
-    this.gold = new Range(100, 2000, this.game)
+    this.gold = new RangeDisplay(100, 2000, this.game)
     this.gold.setText(20, 20, 'gold: ')
     this.gold.setRangePos(70, 10, 32 * 3, 20)
-    this.magie = new Range(0, 2000, this.game)
+    this.magie = new RangeDisplay(0, 2000, this.game)
     this.magie.setText(7 * 32, 20, 'magie: ')
     this.magie.setRangePos(95, 10, 32 * 3, 20)
-    this.taint = new Range(0, 2000, this.game)
+    this.taint = new RangeDisplay(0, 2000, this.game)
     this.taint.setText(430, 20, 'taint: ')
     this.taint.setRangePos(80, 10, 32 * 3, 20)
     this.taint.hide()
-    this.plant = new Range(0, 2000, this.game)
+    this.plant = new RangeDisplay(0, 2000, this.game)
     this.plant.setText(600, 20, 'new plant: ')
     this.plant.setRangePos(150, 10, 32 * 3, 20)
     this.plant.hide()
