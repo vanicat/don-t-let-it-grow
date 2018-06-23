c26ffcfd1bb68ada6e28c93b131eff63d3e1d9b3
Rémi Vanicat <vanicat@debian.org>
2018-06-23 12:05:33 +0200

    create game stuff

--- src/preload.js
+++ src/preload.js
@@ -7,6 +7,7 @@ preload.prototype = {
     loadingBar.anchor.setTo(0.5, 0.5)
     this.load.setPreloadSprite(loadingBar)
     // should preload stuff
+    this.load.image('background', 'assets/debug-grid-1920x1920.png')
   },
 
   create: function () {
--- src/theGame.js
+++ src/theGame.js
@@ -4,7 +4,29 @@ var theGame = function (game) {
 theGame.prototype = {
   preload: function () {
   },
+
   create: function () {
-    console.log('game started')
+    this.magie_usage = 0
+    this.magie_available = 0
+    this.plant = 0
+    this.game.add.tileSprite(0, 0, 1920, 1920, 'background')
+    this.game.world.setBounds(0, 0, 1920, 1920)
+    this.cursors = this.game.input.keyboard.createCursorKeys()
+  },
+
+  update: function () {
+    this.plant += this.magie_usage + 1
+
+    if (this.cursors.left.isDown) {
+      this.game.camera.x -= 4
+    } else if (this.cursors.right.isDown) {
+      this.game.camera.x += 4
+    }
+
+    if (this.cursors.up.isDown) {
+      this.game.camera.y -= 4
+    } else if (this.cursors.down.isDown) {
+      this.game.camera.y += 4
+    }
   }
 }
