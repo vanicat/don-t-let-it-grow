26e3ccce23c6b07894c7c88c7dad98046e3fb853
Rémi Vanicat <vanicat@debian.org>
2018-06-23 21:06:05 +0200

    Some refactoring

--- src/theGame.js
+++ src/theGame.js
@@ -1,34 +1,5 @@
-/* global Phaser Range */
+/* global Phaser Range ToolBox */
 const CAMERA_MOVE = 40
-
-const BUTTONS_HEIGHT = 68
-const BUTTONS_WIDTH = 320
-
-var ToolBox = function (game, x, y) {
-  this.game = game
-  this.buttonsSpecification = {}
-  this.buttons = {}
-  this.x = x
-  this.y = y
-  this.next_button_y = y
-}
-
-ToolBox.prototype = {
-  addButton: function (name, action) {
-    this.buttonsSpecification[name] = {
-      x: this.x,
-      y: this.next_button_y,
-      name: name,
-      action: action
-    }
-
-    var b = this.buttons[name] = this.game.add.button(this.x, this.next_button_y, name, action, this.game)
-    b.fixedToCamera = true
-    b.cameraOffset.setTo(this.x, this.next_button_y)
-
-    this.next_button_y += BUTTONS_HEIGHT
-  }
-}
 
 var theGame = function (game) {
 }
@@ -43,24 +14,7 @@ theGame.prototype = {
     this.game.world.setBounds(0, 0, 1920, 1920)
     this.cursors = this.game.input.keyboard.createCursorKeys()
 
-    // gold should be numeric, not range...
-    this.gold = new Range(100, 2000, this.game)
-    this.gold.setText(20, 20, 'gold: ')
-    this.gold.setRangePos(70, 10, 32 * 3, 20)
-
-    this.magie = new Range(0, 2000, this.game)
-    this.magie.setText(7 * 32, 20, 'magie: ')
-    this.magie.setRangePos(95, 10, 32 * 3, 20)
-
-    this.taint = new Range(0, 2000, this.game)
-    this.taint.setText(430, 20, 'taint: ')
-    this.taint.setRangePos(80, 10, 32 * 3, 20)
-    this.taint.hide()
-
-    this.plant = new Range(0, 2000, this.game)
-    this.plant.setText(600, 20, 'new plant: ')
-    this.plant.setRangePos(150, 10, 32 * 3, 20)
-    this.plant.hide()
+    this.createCounter()
 
     // TODO: set position relatively to the window
 
@@ -114,5 +68,24 @@ theGame.prototype = {
         this.game.camera.y -= 4
       }
     }
+  },
+
+  createCounter: function () {
+    // gold should be numeric, not range...
+
+    this.gold = new Range(100, 2000, this.game)
+    this.gold.setText(20, 20, 'gold: ')
+    this.gold.setRangePos(70, 10, 32 * 3, 20)
+    this.magie = new Range(0, 2000, this.game)
+    this.magie.setText(7 * 32, 20, 'magie: ')
+    this.magie.setRangePos(95, 10, 32 * 3, 20)
+    this.taint = new Range(0, 2000, this.game)
+    this.taint.setText(430, 20, 'taint: ')
+    this.taint.setRangePos(80, 10, 32 * 3, 20)
+    this.taint.hide()
+    this.plant = new Range(0, 2000, this.game)
+    this.plant.setText(600, 20, 'new plant: ')
+    this.plant.setRangePos(150, 10, 32 * 3, 20)
+    this.plant.hide()
   }
 }
