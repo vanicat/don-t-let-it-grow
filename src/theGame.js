570d08ce3121f23260e3600e5f4d0e0b66c1a68c
Rémi Vanicat <vanicat@debian.org>
2018-06-24 07:00:10 +0200

    I don't need this.game has often as It was believed

--- src/theGame.js
+++ src/theGame.js
@@ -10,19 +10,19 @@ theGame.prototype = {
 
   create: function () {
     console.log('starting game')
-    this.game.add.tileSprite(0, 0, 1920, 1920, 'background')
-    this.game.world.setBounds(0, 0, 1920, 1920)
-    this.cursors = this.game.input.keyboard.createCursorKeys()
+    this.add.tileSprite(0, 0, 1920, 1920, 'background')
+    this.world.setBounds(0, 0, 1920, 1920)
+    this.cursors = this.input.keyboard.createCursorKeys()
 
     this.createCounter()
 
     // TODO: set position relatively to the window
 
     // for movement based on the mouse
-    this.leftRectangle = new Phaser.Rectangle(0, 0, CAMERA_MOVE, this.game.camera.height)
-    this.rightRectangle = new Phaser.Rectangle(this.game.camera.width - CAMERA_MOVE, 0, CAMERA_MOVE, this.game.camera.height)
-    this.topRectangle = new Phaser.Rectangle(0, 0, this.game.camera.width, CAMERA_MOVE)
-    this.bottomRectangle = new Phaser.Rectangle(0, this.game.camera.height - CAMERA_MOVE, this.game.camera.width, CAMERA_MOVE)
+    this.leftRectangle = new Phaser.Rectangle(0, 0, CAMERA_MOVE, this.camera.height)
+    this.rightRectangle = new Phaser.Rectangle(this.camera.width - CAMERA_MOVE, 0, CAMERA_MOVE, this.camera.height)
+    this.topRectangle = new Phaser.Rectangle(0, 0, this.camera.width, CAMERA_MOVE)
+    this.bottomRectangle = new Phaser.Rectangle(0, this.camera.height - CAMERA_MOVE, this.camera.width, CAMERA_MOVE)
 
     this.tools = new ToolBox(this, 32, 128)
     this.tools.addButton('hall', function () {})
@@ -30,9 +30,9 @@ theGame.prototype = {
     this.tools.addButton('magicshop', function () {})
     this.tools.addButton('search', function () {})
 
-    this.halls = this.game.add.group()
-    this.farms = this.game.add.group()
-    this.magikShops = this.game.add.group()
+    this.halls = this.add.group()
+    this.farms = this.add.group()
+    this.magikShops = this.add.group()
   },
 
   update: function () {
@@ -47,29 +47,29 @@ theGame.prototype = {
 
   moveCamera: function () {
     if (this.cursors.left.isDown) {
-      this.game.camera.x -= 4
+      this.camera.x -= 4
     } else if (this.cursors.right.isDown) {
-      this.game.camera.x += 4
+      this.camera.x += 4
     }
 
     if (this.cursors.up.isDown) {
-      this.game.camera.y -= 4
+      this.camera.y -= 4
     } else if (this.cursors.down.isDown) {
-      this.game.camera.y += 4
+      this.camera.y += 4
     }
 
-    if (this.game.input.activePointer.active) {
-      var x = this.game.input.activePointer.x
-      var y = this.game.input.activePointer.y
+    if (this.input.activePointer.active) {
+      var x = this.input.activePointer.x
+      var y = this.input.activePointer.y
       if (this.leftRectangle.contains(x, y)) {
-        this.game.camera.x -= 4
+        this.camera.x -= 4
       } else if (this.rightRectangle.contains(x, y)) {
-        this.game.camera.x += 4
+        this.camera.x += 4
       }
       if (this.bottomRectangle.contains(x, y)) {
-        this.game.camera.y += 4
+        this.camera.y += 4
       } else if (this.topRectangle.contains(x, y)) {
-        this.game.camera.y -= 4
+        this.camera.y -= 4
       }
     }
   },
@@ -77,17 +77,17 @@ theGame.prototype = {
   createCounter: function () {
     // gold should be numeric, not range...
 
-    this.gold = new NumericDisplay(100, 2000, this.game)
+    this.gold = new NumericDisplay(100, 2000, this)
     this.gold.setText(20, 20, 'gold: ')
     this.gold.setRangePos(200, 10, 32 * 3, 20)
-    this.magie = new RangeDisplay(0, 2000, this.game)
+    this.magie = new RangeDisplay(0, 2000, this)
     this.magie.setText(240, 20, 'magie: ')
     this.magie.setRangePos(100, 10, 32 * 3, 20)
-    this.taint = new RangeDisplay(0, 2000, this.game)
+    this.taint = new RangeDisplay(0, 2000, this)
     this.taint.setText(430, 20, 'taint: ')
     this.taint.setRangePos(80, 10, 32 * 3, 20)
     this.taint.hide()
-    this.plant = new RangeDisplay(0, 2000, this.game)
+    this.plant = new RangeDisplay(0, 2000, this)
     this.plant.setText(600, 20, 'new plant: ')
     this.plant.setRangePos(150, 10, 32 * 3, 20)
     this.plant.hide()
